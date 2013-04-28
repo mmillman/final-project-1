db594c0295ecdcbd0e410476fe4b8489a613402c
Mark Millman <markmillman@gmail.com>
2013-04-28 14:27:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackOverflowClone::Application.config.secret_token = '09cda8f75cf3b725827e55369844db74303d067bb635470aff28c598c129d2db04b6aa5815e4f543563859cc740eb4efe0c672c43c6510fc31b4be7d17e1c6e9'
