cdd6892aa971ac090332556a639213b9f01052a8
Mark Millman <markmillman@gmail.com>
2013-04-28 15:38:21 -0700

    Create devise views and user

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module StackOverflowClone
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise setup step 4: for heroku deployment
+    config.assets.initialize_on_precompile = false
   end
 end
