282b2349d7b5cb3fb8e3a96e8242e31e3c1c5463
Mark Millman <markmillman@gmail.com>
2013-04-29 11:33:48 -0700

    Change new question route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ StackOverflowClone::Application.routes.draw do
   # Devise setup step 2
   root :to => 'static_pages#home'
 
+  match 'questions/ask' => 'questions#new', :as => 'ask_question', :via => :get
   resources :questions, :only => [:create, :index, :new]
 
 end
