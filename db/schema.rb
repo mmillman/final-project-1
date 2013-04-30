f3590817f60dfcc88f1a4c46dc965ee534b8551d
Mark Millman <markmillman@gmail.com>
2013-04-29 17:23:02 -0700

    Create comment model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429220054) do
+ActiveRecord::Schema.define(:version => 20130429223539) do
 
   create_table "answers", :force => true do |t|
     t.text     "body"
@@ -23,6 +23,19 @@ ActiveRecord::Schema.define(:version => 20130429220054) do
 
   add_index "answers", ["answerer_id"], :name => "index_answers_on_answerer_id"
   add_index "answers", ["question_id"], :name => "index_answers_on_question_id"
+
+  create_table "comments", :force => true do |t|
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "questions", :force => true do |t|
     t.string   "title"
