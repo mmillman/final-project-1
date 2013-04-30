501d31b052ccbc7def71dda98511722096094512
Mark Millman <markmillman@gmail.com>
2013-04-30 15:54:38 -0700

    Refactor navbar

--- app/assets/javascripts/stack_underflow.js
+++ app/assets/javascripts/stack_underflow.js
@@ -8,9 +8,23 @@ var SU = {
   initialize: function () {
     console.log("SU initialized");
 
+    SU.installNavBar($('.main-nav'));
+
     new SU.Routers.QuestionsRouter();
     new SU.Routers.UsersRouter();
 
     Backbone.history.start();
+  },
+
+  installNavBar: function ($navbar) {
+    console.log("Installing navbar")
+    var $ul = $('<ul></ul>');
+
+    var link_names = ["Questions", "Tags", "Users", "Unanswered", "Ask Question"];
+    _(link_names).each(function(linkName) {
+      $ul.append('<li>' + linkName + '</li>');
+    });
+
+    $navbar.append($ul);
   }
 }
